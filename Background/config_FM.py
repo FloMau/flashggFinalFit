67cb68c254031846810eddec6605621ec37594fc
Florian Mausolf <florian.mausolf@rwth-aachen.de>
2026-02-13 20:07:22 +0100

    bkg config path updated

--- Background/config_FM.py
+++ Background/config_FM.py
@@ -3,14 +3,13 @@
 backgroundScriptCfg = {
   
   # Setup
-  'inputWS':'/net/data_cms3a-1/mausolf/HttCPAnalysis/finalFitPreparation/outputForFinalFits_04Nov2025/workspaces/Data/ws/allData.root', # location of 'allData.root' file
+  'inputWS':'/net/data_cms3a-1/mausolf/HttCPAnalysis/finalFitPreparation/outputForFinalFits_24Jan2026_CP_penalty_30/workspaces_fiducial/Data/ws/allData.root', # location of 'allData.root' file
   'cats':'auto', # auto: automatically inferred from input ws
   'catOffset':0, # add offset to category numbers (useful for categories from different allData.root files)  
   'ext':'tth_th_analysis', # extension to add to output directory
   'year':'combined', # Use combined when merging all years in category (for plots)
 
   # Job submission options
-  'batch':'local', # [condor,condor_lxplus,SGE,IC,local]
+  'batch':'condor', # [condor,condor_lxplus,SGE,IC,local]
   'queue':'microcentury' # for condor e.g. microcentury
-  
 }
